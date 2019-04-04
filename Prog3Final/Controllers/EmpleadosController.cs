2b7d4bcc03e19c0de2dd8b4ee82eb9a0bba4481e
ManuxGT <imccs05@hotmail.com>
2019-04-04 02:39:32 -0400

    Add project files.

---
+++ Prog3Final/Controllers/EmpleadosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prog3Final;
+
+namespace Prog3Final.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private DBRecursosHumanos db = new DBRecursosHumanos();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleados = db.Empleados.Include(e => e.Cargo1);
+            return View(empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.Cargo = new SelectList(db.Cargos, "Id", "Codigo");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Codigo,Nombre,Apellido,Telefono,genero,Departamento,Cargo,FechaIngreso,Salario,estatus")] Empleado empleado)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                db.Empleados.Add(empleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cargo = new SelectList(db.Cargos, "Id", "Codigo", empleado.Cargo);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Cargo = new SelectList(db.Cargos, "Id", "Codigo", empleado.Cargo);
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Codigo,Nombre,Apellido,Telefono,genero,Departamento,Cargo,FechaIngreso,Salario,estatus")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cargo = new SelectList(db.Cargos, "Id", "Codigo", empleado.Cargo);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = db.Empleados.Find(id);
+            db.Empleados.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Prog3Final/Controllers/VacacionesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prog3Final.Controllers
+{
+    public class VacacionesController : Controller
+    {
+        private DBRecursosHumanos db = new DBRecursosHumanos();
+
+        // GET: Vacaciones
+        public ActionResult Index()
+        {
+            return View(db.Vacaciones.ToList());
+        }
+
+        // GET: Vacaciones/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Vacaciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.PersonList = new SelectList(db.Empleados.Where(m => m.estatus == "Activo"), "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Vacaciones/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Vacacione vacaciones)
+        {
+            try
+            {
+                int CodigoEmpleado = Convert.ToInt32(Request.Form["IdEmpleado"]);
+                var EmpleadoSalida = db.Empleados.Where(m => m.Id == CodigoEmpleado && m.estatus == "Activo").First();
+
+                int IdSalida =  EmpleadoSalida.Id;
+                vacaciones.IdEmpleado = IdSalida;
+                db.Vacaciones.Add(vacaciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch { }
+            ViewBag.NoResultados = "No hay ningún empleado con este código";
+            return View();
+        }
+
+    }
+}
